49a2fd9127fce8acc535745f6638b2bbc48892e7
Florian Höch <florian.hoech@gmx.de>
2015-08-18 01:04:19 +0200

    Insert 'like' number HTML if not yet there

--- js/public_functions.js
+++ js/public_functions.js
@@ -19,10 +19,11 @@ jQuery(function() {
                 /*console.debug(response);*/
                 button.removeClass('like-loading').addClass('liked');
                 var likes_number = button.parent('.bbpl_button_wrapper').children('.bbpl_number');
-                if(likes_number){
+                if(likes_number.length){
                     var counter = Number(likes_number.text()) + 1;
                     likes_number.text('' + counter + '');
                 }
+                else button.parent('.bbpl_button_wrapper').append('<span class="bbpl_number">' + counter + '</span>');
             });
             
         });
